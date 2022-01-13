b00ecae15bcf504a2e1262f4f8758f272f177522
Prabin-Kumar-Baniya-NP <pra44binind@gmail.com>
2022-01-13 11:36:13 +0545

    JDK, JRE, JVM, Varibles, Data Types in Java

---
+++ 0.0 Basics/0.2 VariablesDataTypes.java
@@ -0,0 +1,19 @@
+class VariablesDataTypes {
+    public static void main(String[] args) {
+        // Primitive Data Types - Total 8
+        byte num1 = 100; // 1 byte
+        short num2 = 5000; // 2 bytes
+        int num3 = 100000; // 4 bytes
+        long num4 = 1000000; // 8 bytes
+        float num5 = 5.55f; // 4 bytes
+        double num6 = 26.567d; // 8 bytes
+        boolean isNum = true; // 1 bit
+        char c = 'j'; // 2 bytes
+        // Non Primitive Data types
+        System.out.println("Class");
+        System.out.println("Object");
+        System.out.println("Array");
+        System.out.println("String");
+        System.out.println("Interface");
+    }
+}
