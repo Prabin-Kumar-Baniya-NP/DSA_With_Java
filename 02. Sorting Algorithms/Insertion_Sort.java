7cfd1809f02e905cb9766125afbf1532ca6d0c3e
Prabin-Kumar-Baniya-NP <pra44binind@gmail.com>
2022-02-01 10:58:10 +0545

    Sorting Techniques

--- 02. Sorting Algorithms/Insertion_Sort.java
+++ 02. Sorting Algorithms/Insertion_Sort.java
@@ -1,30 +1,29 @@
-class Insertion_Sort {
-	public void sort(int array[])
-	{
-		int n = array.length;
-		for (int i =1;i < n; i++)
-		{
-			int key = array[i];
-			int j = i-1;
+// Insertion Sort Time Complexity: O(n^2)
+// Insert an element from an unsorted array to its correct position in sorted array
 
-			while(j>=0 && array[j] > key)
-			{
-				array[j+1] = array[j];
+class Insertion_Sort {
+	public void sort(int array[]) {
+		int n = array.length;
+		for (int i = 1; i < n; i++) {
+			int key = array[i];
+			int j = i - 1;
+
+			while (j >= 0 && array[j] > key) {
+				array[j + 1] = array[j];
 				j = j - 1;
 			}
 			array[j + 1] = key;
 		}
 	}
-	
-	public void print_array(int[] array)
-	{
-		for(int element: array)
-		{
+
+	public void print_array(int[] array) {
+		for (int element : array) {
 			System.out.print(element + " ");
 		}
 	}
+
 	public static void main(String[] args) {
-		int[] array = {6,2,9,1,7,5,4,3,0};
+		int[] array = { 6, 2, 9, 1, 7, 5, 4, 3, 0 };
 		Insertion_Sort obj = new Insertion_Sort();
 		System.out.println("Before Sorting");
 		obj.print_array(array);
--- 02. Sorting Algorithms/Selection_Sort.java
+++ 02. Sorting Algorithms/Selection_Sort.java
@@ -1,17 +1,17 @@
 // Time Complexity : O(n^2)
 // Auxiliary Space : O(1) 
-class Selection_Sort{
-	void sort(int[] array)
-	{
+// Select the minimum element in the unsorted array and swap it with the element at the begining.
+
+class Selection_Sort {
+	void sort(int[] array) {
 		// One by one we have to move the boundary to unsorted array
-		for(int i = 0; i< (array.length-1); i++)
-		{
+		for (int i = 0; i < (array.length - 1); i++) {
 			// Assume the first index of the current boundary is having minimum value
 			int minimum_index = i;
-			// Check for every element after minimum index whether it holds minimum value or not
-			for(int j = i+1; j < array.length; j++)
-			{
-				if(array[j] < array[minimum_index])
+			// Check for every element after minimum index whether it holds minimum value or
+			// not
+			for (int j = i + 1; j < array.length; j++) {
+				if (array[j] < array[minimum_index])
 					minimum_index = j;
 			}
 			// Swap the element in minimum_index with i
@@ -20,19 +20,17 @@ class Selection_Sort{
 			array[i] = temp;
 		}
 	}
+
 	// Method to print array
-	void print_array(int[] array)
-	{
-		for(int element: array)
-		{
+	void print_array(int[] array) {
+		for (int element : array) {
 			System.out.print(element + " ");
 		}
 	}
 
-	public static void main(String args[])
-	{
+	public static void main(String args[]) {
 		Selection_Sort obj = new Selection_Sort();
-		int array[] = {9,1,3,8,4,0,2,7};
+		int array[] = { 9, 1, 3, 8, 4, 0, 2, 7 };
 		System.out.println("Before Sorting");
 		obj.print_array(array);
 		obj.sort(array);
