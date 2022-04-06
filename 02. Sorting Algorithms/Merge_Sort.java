71e5e927316345297ca1788db8746e1b19335171
Prabin-Kumar-Baniya-NP <pra44binind@gmail.com>
2022-04-06 11:39:57 +0530

    Added Merge Sort

--- 02. Sorting Algorithms/Merge_Sort.java
+++ 02. Sorting Algorithms/Merge_Sort.java
@@ -1,70 +1,51 @@
 class Merge_Sort{
-	// merges two subarrays of array[]
-	void merge(int array[], int left, int midpoint, int right){
-		// Find the size of two subarrays to be merged
-		int left_size = midpoint - left + 1;
-		int right_size = right - midpoint;
+	public void conquer(int[] arr, int startIndex, int midIndex, int endIndex) {
+        // Create a new array to storing the storted elements
+        int mergedArray[] = new int[endIndex - startIndex + 1];
+        // Create an iterator i1 for first array
+        int i1 = startIndex;
+        // Create an iterator i2 for second array
+        int i2 = midIndex + 1;
+        // Create an iterator m for mergedArray iteration
+        int m = 0;
 
-		// Create Two Temporary Arrays
-		int left_array[] = new int[left_size];
-		int right_array[] = new int[right_size];
+        // Run while loop till i1 and i2 reaches their end
+        while (i1 <= midIndex && i2 <= endIndex) {
+            // Compare the elements from both array and put the smallest element in merged array
+            if (arr[i1] <= arr[i2]) {
+                mergedArray[m++] = arr[i1++];
+            } else {
+                mergedArray[m++] = arr[i2++];
+            }
+        }
+        // If there are any remaining elements in first array, put them in merged array 
+        while (i1 <= midIndex) {
+            mergedArray[m++] = arr[i1++];
+        }
+        // If there are any remaining elements in second array, put them in merged array 
+        while (i2 <= endIndex) {
+            mergedArray[m++] = arr[i2++];
+        }
+        // Put the elements of merged array into original array
+        // j starts from startIndex beacase start Index of original array is varrying
+        for (int i = 0, j = startIndex; i < mergedArray.length; i++, j++) {
+            arr[j] = mergedArray[i];
+        }
+    }
 
-		// Copying the data from array to the tempoaray arrays
-		for(int i = 0; i < left_size; i++){
-			left_array[i] = array[left + i];
-		}
-		for(int j =0; j < right_size; j++)
-		{
-			right_array[j] = array[midpoint+1+j];
-		}
-
-		// Merging the temporary array
-
-		// Setting the initial index of left and right sub arrays
-		int i = 0, j =0;
-
-		// Initial Index of merged subarray 
-		int k = left;
-
-		while(i < left_size && j < right_size){
-			if(left_array[i] <= right_array[j]){
-				array[k] = left_array[i];
-				i++;
-			}
-			else{
-				array[k] = right_array[j];
-				j++;
-			}
-			k++;
-		}
-		// Copy remaining elements of left_array[] if any
-		while(i < left_size){
-			array[k] = left_array[i];
-			i++;
-			k++;
-		}
-
-		// Copy remaining elements of right_array[] if any
-		while(j< right_size)
-		{
-			array[k] = right_array[j];
-			j++;
-			k++;
-		}
-	}
-	void sort(int array[], int left, int right){
-		if (left < right){
-			// Find the middle point
-			int midpoint = (left + right) /2;
-
-			// Sort first and second halves
-			sort(array, left, midpoint);
-			sort(array, midpoint+1, right);
-
-			// Merge the sorted Halves
-			merge(array, left, midpoint, right);
-		}
-	}
+    public void divide(int[] arr, int startIndex, int endIndex) {
+        if (startIndex >= endIndex) {
+            return;
+        }
+        // Find the midIndex to divide the array into two parts
+        int midIndex = (startIndex + endIndex) / 2;
+        // First Part of array will be from startIndex to midIndex
+        divide(arr, startIndex, midIndex);
+        // Second Part of array will be from midIndex + 1 to endIndex
+        divide(arr, midIndex + 1, endIndex);
+        // After dividing tha array, conquer the array i.e. start merging
+        conquer(arr, startIndex, midIndex, endIndex);
+    }
 	static void printArray(int array[])
 	{
 		for(int element: array){
@@ -76,7 +57,7 @@ class Merge_Sort{
 		System.out.println("Given Array");
 		printArray(array);
 		Merge_Sort obj = new Merge_Sort();
-		obj.sort(array, 0, array.length-1);
+		obj.divide(array, 0, array.length-1);
 		System.out.println("\nSorted Array");
 		printArray(array);
 	}
--- 02. Sorting Algorithms/Sorting.md
+++ 02. Sorting Algorithms/Sorting.md
@@ -76,3 +76,55 @@ public void sort(int array[]) {
 		}
 	}
 ```
+
+## 4. Merge Sort
+
+```
+public static void conquer(int[] arr, int startIndex, int midIndex, int endIndex) {
+        // Create a new array to storing the storted elements
+        int mergedArray[] = new int[endIndex - startIndex + 1];
+        // Create an iterator i1 for first array
+        int i1 = startIndex;
+        // Create an iterator i2 for second array
+        int i2 = midIndex + 1;
+        // Create an iterator m for mergedArray iteration
+        int m = 0;
+
+        // Run while loop till i1 and i2 reaches their end
+        while (i1 <= midIndex && i2 <= endIndex) {
+            // Compare the elements from both array and put the smallest element in merged array
+            if (arr[i1] <= arr[i2]) {
+                mergedArray[m++] = arr[i1++];
+            } else {
+                mergedArray[m++] = arr[i2++];
+            }
+        }
+        // If there are any remaining elements in first array, put them in merged array 
+        while (i1 <= midIndex) {
+            mergedArray[m++] = arr[i1++];
+        }
+        // If there are any remaining elements in second array, put them in merged array 
+        while (i2 <= endIndex) {
+            mergedArray[m++] = arr[i2++];
+        }
+        // Put the elements of merged array into original array
+        // j starts from startIndex beacase start Index of original array is varrying
+        for (int i = 0, j = startIndex; i < mergedArray.length; i++, j++) {
+            arr[j] = mergedArray[i];
+        }
+    }
+
+    public static void divide(int[] arr, int startIndex, int endIndex) {
+        if (startIndex >= endIndex) {
+            return;
+        }
+        // Find the midIndex to divide the array into two parts
+        int midIndex = (startIndex + endIndex) / 2;
+        // First Part of array will be from startIndex to midIndex
+        divide(arr, startIndex, midIndex);
+        // Second Part of array will be from midIndex + 1 to endIndex
+        divide(arr, midIndex + 1, endIndex);
+        // After dividing tha array, conquer the array i.e. start merging
+        conquer(arr, startIndex, midIndex, endIndex);
+    }
+```
